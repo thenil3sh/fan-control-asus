f95400f5936bb25107e75ee9a87ac157564515c7
thenil3sh <nileshkumar.newdelhi@gmail.com>
2025-07-05 23:35:34 +0530

    able to grab unchanged properties

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,17 @@
-use std::env::Args;
+// use std::env::Args;
 
-pub struct Modes {
-    no_auto: bool,
-    fan_mode: u8,
-    force: bool,
-}
+// pub struct Modes {
+//     no_auto: bool,
+//     fan_mode: u8,
+//     force: bool,
+// }
 
-impl Modes {
-    pub fn get(mut args_vec: Args) -> Self {
-        for args in args_vec.next() {}
-        todo!()
-    }
-}
+// impl Modes {
+//     pub fn get(mut args_vec: Args) -> Self {
+//         // for args in args_vec.next() {}
+//         todo!("Parse your args into modes");
+//         todo!("Check, if dbus already has control on your fan profiles");
+//     }
+// }
+
+// Nothings happening here
--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,70 @@
+// use fan_control_asus::Modes;
 use futures_util::StreamExt;
 use smol::block_on;
+use std::fs::{File, OpenOptions};
+use std::io::Write;
 use zbus::Connection;
 use zbus::fdo::PropertiesProxy;
 use zbus::names::InterfaceName;
 
 fn main() {
+    // let args = std::env::args();
+
+    // let modes = Modes::get(args);
     let connection = block_on(async { Connection::system().await }).unwrap();
     let service_name = "org.freedesktop.UPower";
-    let root_path = "/org/freedesktop/UPower/devices/line_power_AC0";
+    let device_path = "/org/freedesktop/UPower/devices/line_power_AC0";
 
     let proxy = block_on(async {
         PropertiesProxy::builder(&connection)
             .destination(service_name)
             .unwrap()
-            .path(root_path)
+            .path(device_path)
             .unwrap()
             .build()
             .await
     })
     .unwrap();
+    let mut thermal_throttle_policy = OpenOptions::new()
+        .write(true)
+        .open("/sys/devices/platform/asus-nb-wmi/throttle_thermal_policy")
+        .unwrap();
 
     let interface = InterfaceName::from_static_str("org.freedesktop.UPower.Device").unwrap();
+
+    block_on(async {
+        let charging_status: bool = proxy
+            .get(interface.clone(), "Online")
+            .await
+            .unwrap()
+            .try_into()
+            .unwrap();
+        handle_ttp(charging_status, &mut thermal_throttle_policy);
+    });
 
     let mut prop_stream = block_on(async { proxy.receive_properties_changed().await }).unwrap();
 
     block_on(async {
         while let Some(signal) = prop_stream.next().await {
             let args = signal.args().unwrap();
-            if &args.interface_name == &interface {
-                if let Some(percentage) = args.changed_properties().get("Online") {
-                    println!("Device is charging : {percentage}");
+            if args.interface_name == interface {
+                if let Some(charging) = args.changed_properties().get("Online") {
+                    handle_ttp(charging.try_into().unwrap(), &mut thermal_throttle_policy);
                 }
             }
         }
     });
 
     todo!("Now track battery level as well, concurrently");
-    todo!("Gain root access to edit /sys/devices/platform/asus-nb-wmi/throttle_thermal_policy");
-    todo!("or maybe use dbus for that, ;)")
+    // todo!("access & edit /sys/devices/platform/asus-nb-wmi/throttle_thermal_policy");
+}
+
+fn handle_ttp(charging: bool, ttp: &mut File) {
+    if charging {
+        ttp.write_all("1\n".as_bytes()).unwrap();
+        println!("Power source : AC, ttp set to Performance")
+    } else {
+        ttp.write_all("0\n".as_bytes()).unwrap();
+        println!("Power source : Battery, ttp set to Normal")
+    }
 }
